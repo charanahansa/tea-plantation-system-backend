f11c8d6f2fbb34227d2259c0395c6a9d966d3d74
Charana Hansa <charanahansa@gmail.com>
2025-01-02 12:30:40 +0530

    report interfaces are created

---
+++ app/Contracts/DataCollectorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Contracts;
+
+interface DataCollectorInterface {
+
+    public function collectData();
+
+}
---
+++ app/Contracts/DataProcessorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Contracts;
+
+interface DataProcessorInterface {
+
+    public function processData($collectedData);
+
+}
